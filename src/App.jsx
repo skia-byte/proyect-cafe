8485a2b0c9dd67acd1da62cf523c849e686dcc4e
kiarasandovalCTC <kiasandovalrosas@crackthecode.la>
2025-10-14 19:31:12 -0500

    feat: add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import React from "react";
-
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import AboutUs from "./pages/AboutUs";
+import ComplaintsBook from "./pages/ComplaintsBook";
+import Menu from "./pages/Menu";
+import MissionAndVision from "./components/MissionAndVision";
+import ContactUs from "./components/ContactUs";
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/complaints" element={<ComplaintsBook />} />
+        <Route path="/mission" element={<MissionAndVision />} />
+        <Route path="*" element={<h1>404 Not Found</h1>} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/contact" element={<ContactUs />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
