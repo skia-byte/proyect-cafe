ce1160d01725b33033b04a84342baec81826c2c6
kiarasandovalCTC <kiasandovalrosas@crackthecode.la>
2025-10-14 19:45:56 -0500

    fix: pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,22 @@ import Home from "./pages/Home";
 import AboutUs from "./pages/AboutUs";
 import ComplaintsBook from "./pages/ComplaintsBook";
 import Menu from "./pages/Menu";
-import MissionAndVision from ".pages/MissionAndVision";
+import MissionAndVision from "./pages/MissionAndVision";
 import ContactUs from ".pages/ContactUs";
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<AboutUs />} />
-        <Route path="/complaints" element={<ComplaintsBook />} />
-        <Route path="/missionandvision" element={<MissionAndVision />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/contact" element={<ContactUs />} />
-        <Route path="*" element={<h1>404 Not Found</h1>} />
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/complaints" element={<ComplaintsBook />} />
+          <Route path="/missionandvision" element={<MissionAndVision />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/contact" element={<ContactUs />} />
+          <Route path="*" element={<h1>404 Not Found</h1>} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
