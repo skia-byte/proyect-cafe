4dce13e562ed14e6e48e9a415658ef8e83f9e492
Mariana <marbricenodelgado@crackthecode.la>
2025-10-14 21:42:47 -0500

    importando navbar y logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
+import logo from "../img/logonav.png";
 export default function Navbar() {
   return (
     <nav className="fixed top-0 left-0 w-full bg-[#fdf2idd] shadow-md">
-      <div className="container mx-auto flex items-center justify-between px-4">
+      <div className="container mx-auto max-w-6xl px-4 py-3 flex items-center justify-between">
+        <img src={logo} alt="Logo" classnmae="h-8" />
         <ul className="flex space-x-4 text-brown-800">
           <li>Inicio</li>
           <li>Sobre nosotras</li>
