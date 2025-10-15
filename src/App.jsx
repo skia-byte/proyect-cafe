2e6030cc6464fb3844d1e8fd1630dac4d0c96b9f
kiarasandovalCTC <kiasandovalrosas@crackthecode.la>
2025-10-14 19:36:52 -0500

    fix: componets

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import Home from "./pages/Home";
 import AboutUs from "./pages/AboutUs";
 import ComplaintsBook from "./pages/ComplaintsBook";
 import Menu from "./pages/Menu";
-import MissionAndVision from "./components/MissionAndVision";
-import ContactUs from "./components/ContactUs";
+import MissionAndVision from ".pages/MissionAndVision";
+import ContactUs from ".pages/ContactUs";
 function App() {
   return (
     <>
@@ -14,9 +14,9 @@ function App() {
         <Route path="/about" element={<AboutUs />} />
         <Route path="/complaints" element={<ComplaintsBook />} />
         <Route path="/missionandvision" element={<MissionAndVision />} />
-        <Route path="*" element={<h1>404 Not Found</h1>} />
         <Route path="/menu" element={<Menu />} />
         <Route path="/contact" element={<ContactUs />} />
+        <Route path="*" element={<h1>404 Not Found</h1>} />
       </Routes>
     </>
   );
