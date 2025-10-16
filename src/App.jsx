a1377201956ee5c01601e2804d6092dbd39e1990
Fabiana <andreafabianamogollonlazaro@gmail.com>
2025-10-15 19:43:47 -0500

    fix: routes

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,22 @@ import ComplaintsBook from "./pages/ComplaintsBook";
 import Menu from "./pages/Menu";
 import MissionAndVision from "./pages/MissionAndVision";
 import ContactUs from "./pages/ContactUs";
-import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<AboutUs />} />
-          <Route path="/complaints" element={<ComplaintsBook />} />
-          <Route path="/missionandvision" element={<MissionAndVision />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/contactus" element={<ContactUs />} />
-          <Route path="*" element={<h1>404 Not Found</h1>} />
-        </Routes>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about-us" element={<AboutUs />} />
+            <Route path="/complaints" element={<ComplaintsBook />} />
+            <Route path="/mission-vision" element={<MissionAndVision />} />
+            <Route path="/menu" element={<Menu />} />
+            <Route path="/contact-us" element={<ContactUs />} />
+            <Route path="*" element={<h1>404 Not Found</h1>} />
+          </Routes>
+        </Layout>
       </BrowserRouter>
     </>
   );
