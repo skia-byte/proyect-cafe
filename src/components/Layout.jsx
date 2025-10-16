fbb2f0d2a12b93bc51d479b2b57e7075faee81de
kiarasandovalCTC <kiasandovalrosas@crackthecode.la>
2025-10-16 15:53:13 -0500

    fix: className

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -7,7 +7,7 @@ const Layout = ({ children }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <div classname="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col">
       <Navbar />
 
       <main className="flex-1 pt-20">{children}</main>
