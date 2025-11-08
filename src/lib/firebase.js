c8e215df79a5978905aad9440916121e238cef10
Fabiana <andreafabianamogollonlazaro@gmail.com>
2025-11-07 20:38:59 -0500

    add: firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAg1qbrtqIV2k4R5dG_LL0U2JJvkkzJg-w",
+  authDomain: "cafearoma-u4.firebaseapp.com",
+  projectId: "cafearoma-u4",
+  storageBucket: "cafearoma-u4.firebasestorage.app",
+  messagingSenderId: "706944766295",
+  appId: "1:706944766295:web:b9359672f37365e463ee12",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
