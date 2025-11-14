bb4669d5414b4458395583b09c0ee5f3c2ff25c8
Fabiana <andreafabianamogollonlazaro@gmail.com>
2025-11-14 14:33:04 -0500

    feat: add code to firebase file

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,9 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAg1qbrtqIV2k4R5dG_LL0U2JJvkkzJg-w",
   authDomain: "cafearoma-u4.firebaseapp.com",
@@ -13,5 +11,8 @@ const firebaseConfig = {
   appId: "1:706944766295:web:b9359672f37365e463ee12",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+export const db = getFirestore(app);
