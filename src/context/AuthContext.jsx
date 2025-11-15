113c3623a16a308092fb0fa84c4592137a6d5006
Fabiana <andreafabianamogollonlazaro@gmail.com>
2025-11-14 21:08:40 -0500

    feat: authentication with google

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -8,3 +8,29 @@ import {
   signOut,
   onAuthStateChanged,
 } from "firebase/auth";
+
+//Autenticación con Google
+const AuthContext = createContext();
+
+const ADMIN_EMAIL = ["fablazaromogollon@crackthecode.la"];
+
+export const AuthProvider = () => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser || null);
+      setLoading(false);
+    });
+    return () => unsub();
+  }, []);
+
+  const loginWithGoogle = async () => {
+    await signInWithPopup(auth, googleProvider);
+  };
+
+  const logout = async () => {
+    await signOut(auth);
+  };
+};
